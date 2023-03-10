12b24499bec09ac7af1c4fb725fba62cabfbd29f
Simon Davis <s.davis@hotmail.co.nz>
2023-03-10 10:35:29 +0000

    stylised nav bar with highlighting

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,17 @@
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 
 
 const NavBar = () => {
 
+  const location = useLocation()
+
+  useEffect(() => {
+    console.log(location)
+  }, [location])
 
   return (
 
@@ -14,15 +20,15 @@ const NavBar = () => {
       <Navbar expand="md">
         <Nav variant="Pills" defaultActiveKey="/" expand="md" >
           <Nav.Item>
-            <Nav.Link to="/" as={Link}>Home</Nav.Link>
+            <Nav.Link to="/" as={Link} className={location.pathname === '/' ? 'active' : ''}>Home</Nav.Link>
           </Nav.Item>
         </Nav>
         <Nav variant="Pills" defaultActiveKey="/" expand="md" id="second" >
           <Nav.Item>
-            <Nav.Link to="/pc" as={Link}>All PC Games</Nav.Link>
+            <Nav.Link to="/pc" as={Link} className={location.pathname === '/pc' ? 'active' : ''}>All PC Games</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link to="/browser" as={Link}>All Broswer Games</Nav.Link>
+            <Nav.Link to="/browser" as={Link} className={location.pathname === '/browser' ? 'active' : ''}>All Broswer Games</Nav.Link>
           </Nav.Item>
         </Nav>
       </Navbar>
