71f1c899331653b78f17ee58b65fa931748be940
Simon Davis <s.davis@hotmail.co.nz>
2023-03-10 13:08:46 +0000

    working slideshow

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { useParams, Link, useNavigate } from 'react-router-dom'
+import { Slide, Fade } from 'react-slideshow-image'
 
 //!BOOTSTRAP COMPONENTS
 import Container from 'react-bootstrap/Container'
@@ -11,7 +12,9 @@ const GamePage = () => {
 
   const { gameID } = useParams()
 
-  const [ game, setGame ] = useState([])
+  const [game, setGame] = useState([])
+
+  const [pictures, setPictures] = useState([])
 
   //!On mount
 
@@ -21,27 +24,47 @@ const GamePage = () => {
     const getGame = async () => {
       try {
         const { data } = await axios.get(`/api/game?id=${gameID}`)
-        //console.log('RESPONSE', data)
+        const thumbnail = data.thumbnail
         setGame(data)
+        const picsArray = data.screenshots
+        const picsToUse = picsArray.map(pic => {
+          return pic.image
+        })
+        picsToUse.unshift(thumbnail)
+        setPictures(picsToUse)
+
       } catch (err) {
         console.log(err.message)
       }
     }
 
     getGame()
+
   }, [gameID])
 
   return (
     <main>
       <Container className="gameSingle">
         <Row>
-          {game && 
+          {game &&
             <>
               <Col xs="12">
                 <h1 className="display-4 mb-4 mt-3 game-title">{game.title}</h1>
               </Col>
               <Col md="4">
-                <img src={game.thumbnail} alt={game.title} className="thumbnail"></img>
+
+                {/* <img src={game.thumbnail} alt={game.title} className="thumbnail"></img> */}
+
+                <div className="slide-container">
+                  <Fade>
+                    {pictures.map((pic, index) => (
+                      <div key={index}>
+                        <img style={{ width: '100%' }} src={pic} />
+                      </div>
+                    ))}
+                  </Fade>
+                </div>
+
                 <p><span>Genre:</span> {game.genre}</p>
                 <p><span>Platforms:</span> {game.platform}</p>
                 <p><span>Developer:</span> {game.developer}</p>
